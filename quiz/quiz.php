d0b58d38b0f9c71570e21310d3469c1abcdcc46c
Alex Bohlken <alexbohlken@gmail.com>
2017-09-25 13:57:38 -0500

    Php file accurately gets the form info

---
+++ quiz/quiz.php
@@ -0,0 +1,13 @@
+<?php
+	echo "<html><body>";
+
+	# get values of quiz
+	$q1 = $_POST["q1"];
+	$q2 = $_POST["q2"];
+	$q3 = $_POST["q3"];
+	$q4 = $_POST["q4"];
+	$q5 = $_POST["q5"];
+
+	echo "<p>" . $q1 . "</p>";
+	echo "</body></html>";
+?>
