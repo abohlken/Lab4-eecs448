309bc88a9fc41b0517f71d66169268122fc1e862
Alex Bohlken <alexbohlken@gmail.com>
2017-10-01 16:10:49 -0500

    finished bug fixing the form checker

--- shop/formChecker.js
+++ shop/formChecker.js
@@ -1,3 +1,40 @@
 function formCheck()
 {
+	var uName = document.getElementById("username").value;
+	var pass = document.getElementById("password").value;
+	var foundAtSign = false;
+	var foundDot = false;
+	
+	if(uName == "" || password == "")
+		document.write("Username and password fields cannot be blank!");
+	else
+	{
+		for(let i=0;i<uName.length;i++)
+		{
+			//check if valid email address
+			if(!foundAtSign)
+			{
+				if(uName.charAt(i)=="@")
+					foundAtSign = true;
+			}
+			else
+			{
+				if(!foundDot)
+				{
+					if(uName.charAt(i)==".")
+					{
+						if(uName.substring(i,i+4) == ".com")
+							foundDot = true;
+						else
+							document.write("Username must end in .com! ");
+					}
+				}
+			}
+		}
+		
+		if(!(foundDot&&foundAtSign))
+		{
+			document.write("Invalid username!");
+		}
+	}
 }
