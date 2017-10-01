6d2db17422e2a4be8b322f9ecd1b0f2baa194e19
Alex Bohlken <alexbohlken@gmail.com>
2017-10-01 17:06:51 -0500

    Handled error where no game is chosen to buy

--- shop/formChecker.js
+++ shop/formChecker.js
@@ -2,11 +2,16 @@ function formCheck()
 {
 	var uName = document.getElementById("username").value;
 	var pass = document.getElementById("password").value;
+	var ffxiv = document.getElementById("ffxiv").value;
+	var ow = document.getElementById("overwatch").value;
+	var bloodborne = document.getElementById("bloodborne").value;
 	var foundAtSign = false;
 	var foundDot = false;
 	
 	if(uName == "" || password == "")
 		document.write("Username and password fields cannot be blank!");
+	else if(ffxiv == 0&&ow == 0&&bloodborne == 0)
+		document.write("Please choose at least one game to buy!");
 	else
 	{
 		for(let i=0;i<uName.length;i++)
